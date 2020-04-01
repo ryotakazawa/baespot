911c3681bd0fbd043b44e6f18af6e5b914e81f66
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-04-01 21:25:56 +0900

    created user views

---
+++ app/javascript/components/user/UserDetailPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <dl>
+    <dt>ID</dt>
+    <dd>{{ user.id }}</dd>
+    <dt>Email</dt>
+    <dd>{{ user.email }}</dd>
+  </dl>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      user: {}
+    }
+  },
+  mounted () {
+    axios
+      .get(`/api/v1/users/${this.$route.params.id}.json`)
+      .then(response => (this.user = response.data))
+  }
+}
+</script>
+
+<style scoped>
+</style>
