c42dfd73c9a44fffe09e6e87c216ecd828338d29
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-04-01 21:25:07 +0900

    updated app.vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,32 +1,37 @@
 <template>
   <v-app id="app">
     <Header/>
-    <h1>This is app.vue</h1>
-    <p>{{ message }}</p>
-    <Top/>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import Header from "./components/Header";
-import Top from "./components/Top";
+import UserIndexPage from './components/user/UserIndexPage.vue';
+import UserDetailPage from './components/user/UserDetailPage.vue';
+
+const router = new VueRouter({
+  routes: [
+    { path: '/users/index',
+      component: UserIndexPage  },
+    { path: '/users/:id(\\d+)',  // :id は数値のみに制限する
+      component: UserDetailPage  }
+  ]
+})
+
+// ref. https://jp.vuejs.org/v2/guide/plugins.html#%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%81%AE%E4%BD%BF%E7%94%A8
+Vue.use(VueRouter);
 
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  },
+  router,
   components: {
-    Header,
-    Top,
+    Header
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
